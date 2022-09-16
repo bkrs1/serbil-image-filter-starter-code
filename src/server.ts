f2d1cf80901432efa2fbffa8540863e5e3ead544
bilkor <bikoroser@gmail.com>
2022-09-16 16:24:28 +0100

    ajout de la fonction de filtrage

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import { Router, Request, Response } from 'express';
 import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
 (async () => {
@@ -30,7 +31,22 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
   /**************************************************************************** */
 
   //! END @TODO1
-  
+  app.get('/filteredimage', async (req: Request, res: Response)=>{
+    const image_url = req.query.image_url.toString();
+    if(!image_url){
+      res.status(400).send('Une Url est requise');
+
+      const filtered_image = await filterImageFromURL(image_url);
+
+      res.status(200).sendFile(filtered_image, () =>{
+        deleteLocalFiles([filtered_image]);
+      })
+    }
+
+  })
+
+
+
   // Root Endpoint
   // Displays a simple message to the user
   app.get( "/", async ( req, res ) => {
